d844b081cfe3df167090009a89b09bf69fc24779
mahmudsagartechforing <mahmudsagar.techforing@gmail.com>
2021-10-20 18:28:52 +0600

    cyber crime investigator fix

---
+++ scripts/navEffect.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector(".menu-collapse")
+const menuContainer = document.querySelector("nav")
+const menuHeader = document.querySelector("header")
+
+menuBtn.addEventListener("click", () => {
+    menuContainer.classList.toggle("show-on-mobile")
+})
+const yPosition = document.querySelector(".jumbotron--body-text").getBoundingClientRect().top
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= yPosition) {
+        menuHeader.classList.add("sticky-header")
+    } else
+        menuHeader.classList.remove("sticky-header")
+})
